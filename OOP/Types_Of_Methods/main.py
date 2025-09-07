2016a8a1ebef9131c8780ab4b89c92a587efb352
Anurag Pant <anuragpant26@gmail.com>
2025-09-07 16:44:42 +0530

    Types of methods

---
+++ OOP/Types_Of_Methods/main.py
@@ -0,0 +1,27 @@
+# static , class , instance
+class Student():
+    school ="KKSN"
+
+    def __init__(self,m1,m2,m3):
+        self.m1=m1
+        self.m2=m2
+        self.m3=m3
+
+    def avg(self):   # instance method and we call it using s1.avg()
+        return (self.m1+self.m2+self.m3)/3
+    @classmethod
+    def getSchoolName(cls):
+        cls.school="Hello"
+        
+        return cls.school
+    @staticmethod
+    def info():
+        print("This is Student Class..",Student.school)
+    
+
+
+s1=Student(23,23,24)
+s2=Student(32,32,12)
+print(s1.avg(),s2.avg())
+print(Student.info()) # it shows error we need to pass something thats why we will use decprator 
+Student.info()
