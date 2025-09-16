1bbe5c8f7debf59d1f9a5dce3acab3f75db5c770
Anurag Pant <anuragpant26@gmail.com>
2025-09-16 17:20:34 +0530

    List in python

---
+++ Datastructures/list.py
@@ -0,0 +1,22 @@
+# list define = list is a lianer data strcuture in python which is  mutable in nature is used to store the data of same or different data types
+
+name = ['a','b','c']
+
+# accessing list
+# with index 
+
+print(name[0])
+print(name[1])
+print(name[2])
+
+# with for loop 
+
+for i in name:
+    print(i)
+
+print(name, type(name))
+# change element
+name[0]="A"
+
+
+print(name, type(name))
